f2e826854005744c7d1f1ef034f74bbda7e2c7c9
Lazuardi N Putra <me@laznp.id>
2024-10-04 13:47:19 +0700

    update to match my keyboard

--- chattering_fix.sh
+++ chattering_fix.sh
@@ -1,3 +1,4 @@
-#!bin/bash
+#!/usr/bin/env bash
 # Change the line below to the absolute path of the folder
-cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
+# cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
+cd /home/laznp/Projects/personal/KeyboardChatteringFix-Linux && sudo python3 -m src -k usb-KBDKSP_Iteung_vial:f64c2b3c-event-kbd
