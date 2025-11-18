23338496e35c5d843bb42730af36cde6e19991b2
Lazuardi N Putra <me@laznp.id>
2025-11-19 02:39:51 +0700

    master update

--- chattering_fix.sh
+++ chattering_fix.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Change the line below to the absolute path of the folder
 # cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
-cd /home/laznp/Projects/personal/KeyboardChatteringFix-Linux && sudo python3 -m src -k usb-KBDKSP_Iteung_vial:f64c2b3c-event-kbd
+cd /home/laznp/Projects/personal/KeyboardChatteringFix-Linux && sudo python3 -m src -k usb-CRKBD_Corne_Keyboard-if01-event-kbd -t 30
+# cd /home/laznp/Projects/personal/KeyboardChatteringFix-Linux && sudo python3 -m src
